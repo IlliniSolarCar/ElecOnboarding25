d08c26ea0b7c47db4bf2941f75ed7f4ca3dcc4f2
Nathan Wang <nathanwang2016@gmail.com>
2025-09-16 19:46:58 -0500

    FinishedFirmwareProject1
    Made ledblink digitalout object and added code to turn it off and on like a heartbeat. Not sure how to run/debug to test.

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -4,7 +4,7 @@
  */
 
 #include <mbed.h>
-// PROJECT 1 - Include something here!
+#include <setup.h>
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -41,7 +41,7 @@ void setup() {
 	//set up the CAN interrupts and handling.
 	common.setupCAN();
 	//set up LEDs and turn them all off
-	common.setupLEDs(&led1, &led2, &led3, &led4);
+	common.setupLEDs(&led1, &led2, &led3, &led4, &ledblink);
 
 	//Set Callbacks:
 	//These are side tasks (up to 8) that are run independently of the main
@@ -95,8 +95,13 @@ int main() {
         	common.toggleReceiveCANLED();
         }
 
-        if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
-        	//PROJECT 1 - add code here to actually make the LED blink
+        if(timing.tickThreshold(last_task_1_time, BLINK_RATE_US)){
+        	if(ledblink.read() == 0){
+        		ledblink.write(1);
+        	}
+        	else{
+        		ledblink.write(0);
+        	}
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
