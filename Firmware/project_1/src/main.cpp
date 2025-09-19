ecfd05e7791a28282b3041fe44d56748ffcf9d4b
Maxie Xie <Maxie@wirelessprv-10-195-84-119.near.illinois.edu>
2025-09-18 20:31:52 -0500

    easy projects done

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -10,6 +10,7 @@
 #include "CAN/can_id.h"
 #include "CAN/can_data.h"
 #include "can_buffer.h"
+#include "DigitalOut.h"
 
 
 /*
@@ -97,6 +98,12 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	if (PLED5.read() == 0){
+        		PLED.write(1);
+        	}else{
+        		PLED.write(0);
+        	}
+
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
