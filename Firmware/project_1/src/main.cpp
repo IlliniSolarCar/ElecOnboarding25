38348fee274785f53136a5a67b608ca25cae8d41
Yuan Qian <yuanq4@illinois.edu>
2025-09-23 14:38:17 -0500

    heartbeat project submission

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.h"     // for DigitalOut LED (P_LED)
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -97,6 +98,7 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	LED = !LED;// toggle heart beat LED every interval
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
