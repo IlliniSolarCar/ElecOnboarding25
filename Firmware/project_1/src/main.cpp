14281d19f09048c4c744b3f2331273e9f4321f70
ask42 <emailanishskumar@gmail.com>
2025-09-25 02:06:40 -0500

    Potentiometer Blink Rate Project

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -96,12 +96,15 @@ int main() {
         	common.toggleReceiveCANLED();
         }
 
-        if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
+        //PROJECT 2 - use the potentiometer to change the blink rate
+        float pot_value = pot.read(); //Reads from 0-1
+        uint32_t blink_interval = 500000 + (uint32_t)(1000000 * pot_value); //Converts range from 0-1 to 0.5-1.5s
+
+
+        if(timing.tickThreshold(last_task_1_time, blink_interval)){
         	//PROJECT 1 - add code here to actually make the LED blink
         	led_heartbeat = !led_heartbeat.read();
         }
-
-        //PROJECT 2 - use the potentiometer to change the blink rate
 
 
 	}
