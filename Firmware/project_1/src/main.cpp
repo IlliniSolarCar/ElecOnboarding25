39529bbfee7d44d23edb9a8db74ee851baa20a7f
Riru312 <suyan4321@gmail.com>
2025-09-25 19:57:05 -0500

    Onboarding Firmware Project 1

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -11,7 +11,7 @@
 #include "CAN/can_data.h"
 #include "can_buffer.h"
 
-
+#include <pins.cpp>
 /*
  * This is an example function. It blinks the heartbeat LED and sends
  * a Heartbeat CAN Message. The message sends when the LED turns on.
@@ -41,7 +41,7 @@ void setup() {
 	//set up the CAN interrupts and handling.
 	common.setupCAN();
 	//set up LEDs and turn them all off
-	common.setupLEDs(&led1, &led2, &led3, &led4);
+	common.setupLEDs(&led1, &led2, &led3, &led4, &led5);
 
 	//Set Callbacks:
 	//These are side tasks (up to 8) that are run independently of the main
@@ -95,8 +95,14 @@ int main() {
         	common.toggleReceiveCANLED();
         }
 
-        if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
+        if(timing.tickThreshold(last_task_1_time, LED_BLINK_TIME)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	if (led5.read()){
+        		led5.write(0);
+        	}
+        	else {
+        		led5.write(1);
+        	}
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
