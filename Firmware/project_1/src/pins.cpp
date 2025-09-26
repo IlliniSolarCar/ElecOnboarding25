4b1c44145464fe9f6bb83bf79354d3a5359d09b0
hanqiuh <hanqiuhu@outlook.com>
2025-09-25 19:46:55 -0500

    Add Firmware easy project

--- Firmware/project_1/src/pins.cpp
+++ Firmware/project_1/src/pins.cpp
@@ -22,4 +22,7 @@ DigitalOut led1(P_LED1);
 DigitalOut led2(P_LED2);
 DigitalOut led3(P_LED3);
 DigitalOut led4(P_LED4);
+DigitalOut led_heartbeat(P_LED_HEARTBEAT);
+
 // PROJECT 1 - You can instantiate your DigitalOut object here
+DigitalOut ledHeartbeat(P0_4);
