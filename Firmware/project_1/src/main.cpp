9a4a46bae7ce55f2c5a522f467851b72c4b31840
asmisehra <asmisehra@gmail.com>
2025-09-29 23:43:12 -0500

    Add heartbeat LED logic

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -10,6 +10,7 @@
 #include "CAN/can_id.h"
 #include "CAN/can_data.h"
 #include "can_buffer.h"
+#include "pins.h"
 
 
 /*
@@ -96,6 +97,7 @@ int main() {
         }
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
+        	heartbeat_led = !heartbeat_led;
         	//PROJECT 1 - add code here to actually make the LED blink
         }
 
