f76eedaf0c9a6c7d78ea0bca98591a08e88b3764
Nickjay Saini <nickjay2@illinois.edu>
2025-09-30 20:21:37 -0500

    submitting both firmare projects

--- Firmware/project_1/inc/pins.h
+++ Firmware/project_1/inc/pins.h
@@ -26,6 +26,8 @@
 #define P_LED3 P0_6
 #define P_LED4 P0_7
 // PROJECT 1 - You can define a pin macro here
+#define P_LED_TEST P0_4
+#define P_POTENTIOMETER P0_5
 
 /*
  * COMMON PIN OBJECT DECLARATIONS
@@ -35,13 +37,17 @@ extern DigitalOut led1;
 extern DigitalOut led2;
 extern DigitalOut led3;
 extern DigitalOut led4;
+extern DigitalOut led_test;
 // PROJECT 1 - You can declare a DigitalOut object here
+
 
 /*
  * BOARD SPECIFIC PINS
  */
 
 // PROJECT 2 - You can declare a AnalogIn object here
+extern AnalogIn potentiometer;
+
 
 
 /*
--- Firmware/project_1/src/pins.cpp
+++ Firmware/project_1/src/pins.cpp
@@ -13,6 +13,7 @@
  */
 
 // PROJECT 2 - You can instantiate your AnalogIn object here
+AnalogIn potentiometer(P_POTENTIOMETER)
 
 /*
  * COMMON PIN OBJECT INSTANTIATIONS
@@ -22,4 +23,5 @@ DigitalOut led1(P_LED1);
 DigitalOut led2(P_LED2);
 DigitalOut led3(P_LED3);
 DigitalOut led4(P_LED4);
+DigitalOut led_test(P_LED_Test);
 // PROJECT 1 - You can instantiate your DigitalOut object here
