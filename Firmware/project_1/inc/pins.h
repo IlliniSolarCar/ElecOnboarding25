27aebae9b56d6a8ac07a0db1e93e7eb2a10b76e6
chou45-2 <chou45@illinois.edu>
2025-09-30 19:27:05 -0500

    Kicad and MCUXpresso changes

--- Firmware/project_1/inc/pins.h
+++ Firmware/project_1/inc/pins.h
@@ -25,6 +25,7 @@
 #define P_LED2 P0_5
 #define P_LED3 P0_6
 #define P_LED4 P0_7
+#define P_LED_HEART P0_4
 // PROJECT 1 - You can define a pin macro here
 
 /*
@@ -36,6 +37,7 @@ extern DigitalOut led2;
 extern DigitalOut led3;
 extern DigitalOut led4;
 // PROJECT 1 - You can declare a DigitalOut object here
+extern DigitalOut heart;
 
 /*
  * BOARD SPECIFIC PINS
