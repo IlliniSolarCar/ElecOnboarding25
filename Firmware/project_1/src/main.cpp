f09d687a7a5a4adf5066231ea5402b737e61fb01
Jacob Annes <annes.jacob@gmail.com>
2025-10-01 23:20:31 -0500

    Complete Firmwware Project 1

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -97,6 +98,11 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	if(LED_circ.read()) {
+        		LED_circ.write(1);
+        	} else {
+        		LED_circ.write(0);
+        	}
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
