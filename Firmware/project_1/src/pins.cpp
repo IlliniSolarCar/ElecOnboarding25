31bc6166417aa82c166b6887eee0ffd484bd48e0
Nikhil Gourisetty <140029676+ngourise@users.noreply.github.com>
2025-10-02 00:38:20 -0500

    Update pins.cpp

--- Firmware/project_1/src/pins.cpp
+++ Firmware/project_1/src/pins.cpp
@@ -22,4 +22,6 @@ DigitalOut led1(P_LED1);
 DigitalOut led2(P_LED2);
 DigitalOut led3(P_LED3);
 DigitalOut led4(P_LED4);
-// PROJECT 1 - You can instantiate your DigitalOut object here
+
+// PROJECT 1 - Blink LED instantiation
+DigitalOut led_blink(P_LED_BLINK);
