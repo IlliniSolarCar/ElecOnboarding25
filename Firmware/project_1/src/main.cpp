363fe5ef2a624e2f22268d4d46c9fd168b265e50
Nikhil Gourisetty <140029676+ngourise@users.noreply.github.com>
2025-10-02 00:38:00 -0500

    Update main.cpp

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -95,13 +96,16 @@ int main() {
         	common.toggleReceiveCANLED();
         }
 
-        if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
-        	//PROJECT 1 - add code here to actually make the LED blink
+        if(timing.tickThreshold(last_task_1_time, LED_BLINK_RATE_US)){
+        	// Blink the external LED
+        	led_blink = !led_blink;
+        	if(timing.tickThreshold(last_task_1_time, LED_BLINK_RATE_US)) {
+        	    led_blink = !led_blink;   // toggle LED state
+        	    printf("LED state: %d\n", led_blink.read());
+        	}
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
-
-
 	}
 
 	shutdown_method();
