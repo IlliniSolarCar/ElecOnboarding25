6355462c401b2d4800841676fdb07de983894173
Nikhil Gourisetty <140029676+ngourise@users.noreply.github.com>
2025-10-02 00:37:31 -0500

    Update setup.h

--- Firmware/project_1/inc/setup.h
+++ Firmware/project_1/inc/setup.h
@@ -16,10 +16,7 @@
 //be warned this oscialltor is only accurate +/- 40% so make this time bigger than you think you need
 #define WDT_TIMEOUT_US 10000000
 
-//task 1 rate
-// PROJECT 1 - You can change the macro name to something more descriptive if you'd like
-#define TASK_1_RATE_US 1000000
-
-
+// Blink LED once per second
+#define LED_BLINK_RATE_US 1000000
 
 #endif /* SETUP_H_ */
