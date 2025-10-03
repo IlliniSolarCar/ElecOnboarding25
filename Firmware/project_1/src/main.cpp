8363003aa61d9cf0777fad64f66c667da46073bb
Trevor <trevorzipprich2007@gmail.com.com>
2025-10-02 19:57:17 -0500

    Firmware project1

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -10,6 +10,7 @@
 #include "CAN/can_id.h"
 #include "CAN/can_data.h"
 #include "can_buffer.h"
+#include "mbed/libraries/mbed/api/DigitalOut.h"
 
 
 /*
@@ -97,7 +98,8 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
-        }
+        	LED1 = !LED1.read();}
+
 
         //PROJECT 2 - use the potentiometer to change the blink rate
 
