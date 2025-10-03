00bf5306beace26458ff6a938271737f537170ff
KacperParaniuk <kacparaniuk@gmail.com>
2025-10-03 09:42:37 -0500

    Onboarding Check FIX
    Switched the connector footprints, and rearranged the rectifier diode to protect against current back flow and from high current flowing through the mosfet/tripping the fuse. Re-arranged the PCB layout.

--- Firmware/project_1/src/pins.cpp
+++ Firmware/project_1/src/pins.cpp
@@ -14,6 +14,9 @@
 
 // PROJECT 2 - You can instantiate your AnalogIn object here
 
+AnalogIn resistor(P_Resistor);
+
+
 /*
  * COMMON PIN OBJECT INSTANTIATIONS
  */
@@ -24,3 +27,4 @@ DigitalOut led3(P_LED3);
 DigitalOut led4(P_LED4);
 // PROJECT 1 - You can instantiate your DigitalOut object here
 DigitalOut led5(P_LED5);
+
